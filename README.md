0228be7034ccbad21a9b9f182584dc12a030f1a6
GhunaymY <ghunaym@gmail.com>
2023-09-28 17:32:58 -0600

    Completed  Code

--- README.md
+++ README.md
@@ -24,3 +24,24 @@ examples thus some of the code is Copyright © 2001-2013 Python
 Software Foundation; All Rights Reserved under the PSF license (GPL
 compatible) http://docs.python.org/2/library/socketserver.html
 
+Author
+========================
+* Ghunaym Yahya
+
+Consulted
+========================
+* Afaq Nabi
+
+Sources
+========================
+
+405: "Not Allowed Response" : https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/405#
+404: "Not Found Response": https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/404#
+301: "Moved Permanently" : https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/301
+200: "OK" : https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/200
+
+Mime Types: https://www.baeldung.com/linux/file-mime-types#:~:text=A%20MIME%20type%20consists%20of,multipart%2C%20text%2C%20and%20video.
+
+HTTP Responses: https://developer.mozilla.org/en-US/docs/Web/HTTP/Messages
+OS Path Exists: https://www.geeksforgeeks.org/python-os-path-exists-method/
+OS is Dir: https://www.geeksforgeeks.org/python-os-path-isdir-method/
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-#  coding: utf-8 
 import socketserver
+import os
+
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
 # 
@@ -26,21 +27,82 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+'''
+Status Codes:
+405: "Not Allowed Response" 
+404: "Not Found Response"
+301: "Moved Permanently" 
+200: "OK" 
+'''
 
+HOST, PORT = "localhost", 8080 #Define the host and port where the server will listen for incoming connections.
+
+# Map file extensions to content types
+CONTENT_TYPES = {
+    ".html": "text/html",
+    ".css": "text/css",
+}
+
+# Create a custom request handler by inheriting from BaseRequestHandler
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+    #function to create an HTTP response
+    def create_response(self, status_code, content_type, content):
+        response = f"HTTP/1.1 {status_code}\r\nContent-Type: {content_type}\r\nContent-Length: {len(content)}\r\nConnection: Closed\r\n\r\n{content}" # Construct an HTTP response with the specified status code, content type, and content
+        return response
+
+    # Handle incoming requests
     def handle(self):
-        self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        self.data = self.request.recv(1024).strip() # Receive data from the client and remove leading/trailing whitespace
+        print("Got a request of: %s\n" % self.data)
+
+        decoded_request = self.data.decode('utf-8') #Decode the received data as UTF-8 to work with it as text
+
+        # Split the request into lines to extract the request line
+        request_lines = decoded_request.split("\r\n")
+        request_line = request_lines[0].split()
+        method, path = request_line[0], request_line[1]
+
+        # Check if the request method is not GET (Could be POST, PUT)
+        if method != "GET":
+            response = self.create_response(405, "text/html", "Method Not Allowed") #Throw the 405 Status Code. 
+            self.request.sendall(bytearray(response, 'utf-8'))
+            return
+
+        file_path = "www" + path # Construct the full file path
+
+        # Check if the file exists
+        if not os.path.exists(file_path) or "../" in path:
+            response = self.create_response(404, "text/html", "Page Not Found") #Throw the 404 Status Code. 
+            self.request.sendall(bytearray(response, 'utf-8'))
+            return
+
+        # Check if the file is a directory
+        #For the Requirement: The webserver can return index.html from directories (paths that end in /)
+        if os.path.isdir(file_path):
+            if not path.endswith("/"):
+                # Perform a 301 redirect
+                redirect_location = path + "/"
+                response = f"HTTP/1.1 301 Moved Permanently\r\nLocation: {redirect_location}\r\n\r\n"
+                self.request.sendall(bytearray(response, 'utf-8'))
+                return
+            file_path += "/index.html" # If the path ends with "/", append "index.html" to it
+
+        # Determine the content type based on the file extension
+        file_extension = os.path.splitext(file_path)[1]
+        content_type = CONTENT_TYPES.get(file_extension, "text/html")
+
+        # Read and send the file content
+        with open(file_path, 'r') as f:
+            content = f.read()
+            response = self.create_response(200, content_type, content)
+            self.request.sendall(bytearray(response, 'utf-8'))
+
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 8080
-
     socketserver.TCPServer.allow_reuse_address = True
     # Create the server, binding to localhost on port 8080
     server = socketserver.TCPServer((HOST, PORT), MyWebServer)
-
     # Activate the server; this will keep running until you
     # interrupt the program with Ctrl-C
     server.serve_forever()
+
